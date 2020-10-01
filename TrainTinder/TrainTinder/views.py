b07621968223abdd60186cbb10c2ef003eb7f28f
Milan Tresoor <milan.tresoor@gmail.com>
2020-10-01 08:36:11 +0200

    CHange to pandas

--- TrainTinder/TrainTinder/views.py
+++ TrainTinder/TrainTinder/views.py
@@ -3,12 +3,15 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, json
 from TrainTinder import app
 import pyreadr
+import datetime as dt
+import pandas as pd
 api = Flask(__name__)
 
-
+DEFAULT_TIME = dt.datetime(2018,5,13,9,6,40)
+DEFAULT_PERRON = 'quth1a'
 
 @app.route('/')
 @app.route('/home')
@@ -23,10 +26,12 @@ def home():
 
 
 
-
+@app.route('/')
 @api.route('/request', methods=['GET'])
 def get_teams():
-    result = pyreadr.read_r('TrainTinder/data/ut_2018-05-07.RData') # also works for Rds
-    response = result["data_vloerveld"]
+    df = pd.read_csv("TrainTinder/data/ut_2018-05-07.csv", index_col=1).rename(columns={"Unnamed: 0": 'row'})
+    df.index = pd.to_datetime(df.index, infer_datetime_format=True)
+    response = [dict(perron1=df.loc[DEFAULT_TIME, DEFAULT_PERRON])]
+
     return Response( json.dumps(response),mimetype = "application/json")
 
